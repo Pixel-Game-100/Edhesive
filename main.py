08ee58c7f5b782b836187376287180b4c5d3dfd2
Caleb Grogan <51964732+Pixel-Game-100@users.noreply.github.com>
2019-06-26 07:51:10 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+print ("Let's play Silly Sentences!")
+
+name = input("Enter a name: ")
+adj1 = input("Enter an adjective: ")
+adj2 = input("Enter an adjective: ")
+adverb = input("Enter an adverb: ")
+food1 = input("Enter a food: ")
+food2 = input("Enter another food: ")
+noun = input("Enter a noun: ")
+place = input("Enter a place: ")
+verb = input("Enter a verb: ")
+
+print("\n" + name + " was planning a dream vacation to " + place + ".") 
+print(name + " was especially looking forward to trying the local") 
+print("cuisine, including " + adj1 + " " + food1 + " and " + food2 + ".") 
+ 
+print("\n" + name + " will have to practice the language " + adverb + " to")
+print("make it easier to " + verb + " with people.")
+ 
+print("\n" + name + " has a long list of sights to see, including the")
+print("\n" + noun + " museum and the " + adj2 + " park. ")
